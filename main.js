fa5e6d27888c81de12ab6a6f304f996cb6526578
ShayneSL <shayneluangvisa@gmail.com>
2022-04-19 18:59:39 -0400

    5thDayOfCoding

--- main.js
+++ main.js
@@ -56,3 +56,27 @@ function disemvowel(str) {
     return vowels.indexOf(el.toLowerCase()) == -1;
   }).join('');
 }
+
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return a ** b
+}
+  
+function subt(a,b){
+  return a - b
+}
