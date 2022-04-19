143e787ca4aa0086237bf08c88c1a1755ca2bc61
ShayneSL <shayneluangvisa@gmail.com>
2022-04-18 20:33:43 -0400

    4thDayCoding

--- main.js
+++ main.js
@@ -48,3 +48,11 @@ function getCount(str) {
   }
   return vowelsCount
 }
+
+function disemvowel(str) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+  return str.split('').filter(function(el) {
+    return vowels.indexOf(el.toLowerCase()) == -1;
+  }).join('');
+}
